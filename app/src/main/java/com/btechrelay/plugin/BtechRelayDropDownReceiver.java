f16fdd17f9c6ac3db9479c8f0628f4f08a09e18e
atakmaps <atak@shfly.org>
2026-04-24 18:07:52 -0800

    PTT button sends ping over existing radio TX path

--- app/src/main/java/com/btechrelay/plugin/BtechRelayDropDownReceiver.java
+++ app/src/main/java/com/btechrelay/plugin/BtechRelayDropDownReceiver.java
@@ -233,6 +233,8 @@ public class BtechRelayDropDownReceiver extends DropDownReceiver
             btnPtt.setOnTouchListener((v, event) -> {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        Log.d("BtechRelay.UI", "PTT DOWN");
+                        sendPing();
                         appendLog("PTT DOWN");
                         if (btManager != null) {
                         btManager.testPtt();
@@ -241,6 +243,7 @@ public class BtechRelayDropDownReceiver extends DropDownReceiver
                         return true;
 
                     case MotionEvent.ACTION_UP:
+                        Log.d("BtechRelay.UI", "PTT UP");
                     case MotionEvent.ACTION_CANCEL:
                         appendLog("PTT UP");
                         pttController.stopTransmit();
